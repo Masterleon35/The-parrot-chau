9b24cba7ca8d26e4f5e5e64f8852068ff19fcb91
leon1996 <leon_35@163.com>
2019-06-18 14:44:48 +0800

    增加对药智网的调试

--- scrapy/learn/main.py
+++ scrapy/learn/main.py
@@ -2,5 +2,6 @@ from scrapy.cmdline import execute
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-execute(["scrapy","crawl","haodafu"])
+# execute(["scrapy","crawl","haodafu"])
 # execute(["scrapy","crawl","jobbole"])
+execute(["scrapy","crawl","yaozhi"])
