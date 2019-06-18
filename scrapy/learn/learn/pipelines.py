4b05526853b19f8cbb5c4317d31e55c51874d5cf
leon1996 <leon_35@163.com>
2019-06-18 14:42:23 +0800

    针对不同的item插入不同的mongo表

--- scrapy/learn/learn/pipelines.py
+++ scrapy/learn/learn/pipelines.py
@@ -6,6 +6,7 @@
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import pymongo
 from twisted.internet import defer, reactor
+from learn.items import YaoZhiHospitalItem,YaoZhiClinicItem
 
 class LearnPipeline(object):
     def process_item(self, item, spider):
@@ -31,8 +32,10 @@ class MongoPipeline():
     @defer.inlineCallbacks
     def process_item(self, item, spider):
         out = defer.Deferred()
-        reactor.callInThread(self._insert, item, out)
-        yield out
+        if isinstance(item, YaoZhiHospitalItem):
+            reactor.callInThread(self._insert, item, out)
+        # reactor.callInThread(self._insert, item, out)
+            yield out
         defer.returnValue(item)
 
     def _insert(self, item, out):
@@ -48,19 +51,39 @@ class MongoPipeline():
 
     def close_spider(self,spider):
         self.client.close()
-    #可运行
-    # def open_spider(self,spider):
-    #     self.client = pymongo.MongoClient('localhost',port = 27017)
-    #     db = self.client.admin
-    #     db.authenticate('litianhao', '19960812')
-    #     self.db = self.client['spider_info_haodafu']
-    #
-    # def process_item(self,item,spider):
-    #     # db = self.client.admin
-    #     # db.authenticate('litianhao', '19960812')
-    #     self.db['spider_info_haodafu'].insert_one(item)
-    #     return item
-    #
-    # def close_spider(self,spider):
-    #     self.client.close()
+
+class MongoPipeline1():
+    def __init__(self, crawler):
+        self.mongo_uri = crawler.settings.get("MONGO_URI")
+        self.mongo_db = crawler.settings.get("MONGO_DB")
+        self.col = crawler.settings.get("MONGO_COL_CLN")
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(crawler)
+
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(self.mongo_uri, port = 27017)
+        db = self.client.admin
+        db.authenticate('litianhao', '19960812')
+        self.db = self.client[self.mongo_db]
+
+        # 异步写法
+    @defer.inlineCallbacks
+    def process_item(self, item, spider):
+        out = defer.Deferred()
+        if isinstance(item, YaoZhiClinicItem):
+            reactor.callInThread(self._insert, item, out)
+            yield out
+        defer.returnValue(item)
+
+    def _insert(self, item, out):
+        self.db[self.col].insert_one(dict(item))
+        reactor.callFromThread(out.callback, item)
+
+
+    def close_spider(self, spider):
+        self.client.close()
+
+
 
